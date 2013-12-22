66f83f1653808db12027af0e7d772bd5426b9e99
Vitor Coimbra <vitorcoliveira91@gmail.com>
2013-12-22 11:50:39 -0200

    Mais progressos na InputManager.

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -7,12 +7,45 @@
 
 #include "InputManager.h"
 
-InputManager::InputManager() {
-	// TODO Auto-generated constructor stub
-
+InputManager::InputManager() :
+		mouseState(0), keyStates(NULL), quitGame(false), mouseX(0), mouseY(0) {
 }
 
 InputManager::~InputManager() {
-	// TODO Auto-generated destructor stub
 }
 
+void InputManager::initKeyboard() {
+	for (int i = 0; i < SDL_NUM_SCANCODES; i++) {
+		keyDown[i] = keyUp[i] = 0;
+	}
+}
+
+void InputManager::initMouse() {
+	for (int i = 0; i < N_MOUSE_BUTTONS; i++) {
+		mouseDown[i] = mouseUp[i] = 0;
+	}
+}
+
+void InputManager::update() {
+	SDL_Event event;
+
+	initKeyboard();
+	initMouse();
+
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_KEYDOWN:
+			keyDown[event.key.keysym.scancode] = true;
+			break;
+		case SDL_KEYUP:
+			keyUp[event.key.keysym.scancode] = true;
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			mouseDown[event.button.button] = true;
+			break;
+		case SDL_MOUSEBUTTONUP:
+			mouseUp[event.button.button] = true;
+			break;
+		}
+	}
+}
--- src/InputManager.h
+++ src/InputManager.h
@@ -8,10 +8,50 @@
 #ifndef INPUTMANAGER_H_
 #define INPUTMANAGER_H_
 
+#include "SDLBase.h"
+
+#define N_MOUSE_BUTTONS 7
+
 class InputManager {
-public:
+private:
+	static InputManager* instance;
+
+	Uint32 mouseState;
+	Uint32* keyStates;
+	bool quitGame;
+	int mouseX;
+	int mouseY;
+
+	bool keyDown[SDL_NUM_SCANCODES];
+	bool keyUp[SDL_NUM_SCANCODES];
+	bool mouseDown[N_MOUSE_BUTTONS];
+	bool mouseUp[N_MOUSE_BUTTONS];
+
+	void initKeyboard();
+	void initMouse();
+
 	InputManager();
+public:
 	virtual ~InputManager();
+
+	static InputManager* getInstance() {
+		if (!instance) {
+			instance = new InputManager();
+		}
+		return instance;
+	}
+
+	void update();
+	bool isKeyDown(int key);
+	bool isKeyUp(int key);
+	bool isKeyHeld(int key);
+	bool isLeftMouseDown(int button);
+	bool isLeftMouseUp(int button);
+	bool isLeftMouseHeld(int button);
+	int mousePosX();
+	int mousePosY();
+	bool isMouseInside(SDL_Rect* rect);
+	bool QuitGame();
 };
 
 #endif /* INPUTMANAGER_H_ */
