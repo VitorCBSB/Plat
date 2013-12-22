d6574b610a4fbf7fb26c4065995af517f7b34dc5
Vitor Coimbra <vitorcoliveira91@gmail.com>
2013-12-22 10:27:49 -0200

    Nova classe para tratamento de input, em construcao

---
+++ src/InputManager.cpp
@@ -0,0 +1,18 @@
+/*
+ * InputManager.cpp
+ *
+ *  Created on: 22/12/2013
+ *      Author: vitor
+ */
+
+#include "InputManager.h"
+
+InputManager::InputManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+InputManager::~InputManager() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/InputManager.h
@@ -0,0 +1,17 @@
+/*
+ * InputManager.h
+ *
+ *  Created on: 22/12/2013
+ *      Author: vitor
+ */
+
+#ifndef INPUTMANAGER_H_
+#define INPUTMANAGER_H_
+
+class InputManager {
+public:
+	InputManager();
+	virtual ~InputManager();
+};
+
+#endif /* INPUTMANAGER_H_ */
