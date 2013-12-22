f136e97a6bd8be8d3577e07b44287acb900dd7fd
Vitor Coimbra <vitorcoliveira91@gmail.com>
2013-12-22 09:13:14 -0200

    Adicionadas classes de Rect

---
+++ src/Rect.cpp
@@ -0,0 +1,12 @@
+/*
+ * Rect.cpp
+ *
+ *  Created on: 22/12/2013
+ *      Author: vitor
+ */
+
+#include "Rect.h"
+
+Rect::Rect(float x, float y, float w, float h) : x(x), y(y), w(w), h(h){
+}
+
---
+++ src/Rect.h
@@ -0,0 +1,19 @@
+/*
+ * Rect.h
+ *
+ *  Created on: 22/12/2013
+ *      Author: vitor
+ */
+
+#ifndef RECT_H_
+#define RECT_H_
+
+class Rect {
+public:
+	float x, y;
+	float w, h;
+
+	Rect(float x, float y, float w, float h);
+};
+
+#endif /* RECT_H_ */
