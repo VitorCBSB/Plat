6bcc20d15f9112d740987ad9fe7a1148909de42b
Vitor Coimbra <vitorcoliveira91@gmail.com>
2013-12-24 16:42:12 -0200

    Classes iniciais.

---
+++ src/TileSet.cpp
@@ -0,0 +1,18 @@
+/*
+ * TileSet.cpp
+ *
+ *  Created on: 24/12/2013
+ *      Author: vitor
+ */
+
+#include "TileSet.h"
+
+TileSet::TileSet() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TileSet::~TileSet() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/TileSet.h
@@ -0,0 +1,17 @@
+/*
+ * TileSet.h
+ *
+ *  Created on: 24/12/2013
+ *      Author: vitor
+ */
+
+#ifndef TILESET_H_
+#define TILESET_H_
+
+class TileSet {
+public:
+	TileSet();
+	virtual ~TileSet();
+};
+
+#endif /* TILESET_H_ */
