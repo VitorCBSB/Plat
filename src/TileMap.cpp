eea6d2dfd113a6e09e3819ec3c2aa5d610e172f5
Vitor Coimbra <vitorcoliveira91@gmail.com>
2013-12-25 23:34:35 -0200

    Renderizacao aparentemente concluida.

--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -9,11 +9,49 @@
 
 TileMap::TileMap(std::string fileName, std::string tileSetFileName,
 		int tileWidth, int tileHeight) :
-		tileSet(TileSetPtr(new TileSet(tileSetFileName, tileWidth, tileHeight))) {
-
+		tileSet(
+				TileSetPtr(
+						new TileSet(tileSetFileName, tileWidth, tileHeight))), tileWidth(
+				tileWidth), tileHeight(tileHeight), position(Vector2(0, 0)) {
+	load(fileName);
 }
 
 TileMap::~TileMap() {
-	// TODO Auto-generated destructor stub
 }
 
+void TileMap::load(std::string fileName) {
+	FILE* fp;
+
+	if ((fp = fopen(fileName.c_str(), "r")) == NULL) {
+		fprintf(stderr, "TileMap: arquivo %s nao pode ser aberto.", fileName.c_str());
+		exit(1);
+	}
+
+	for (int k = 0; k < LAYERS; k++) {
+		for (int i = 0; i < LINES; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				fscanf(fp, "%d", &tileMatrix[k][i][j]);
+			}
+		}
+	}
+
+	fclose(fp);
+}
+
+void TileMap::render(float cameraX, float cameraY) {
+	Vector2 actualPosition(position.x - cameraX, position.y - cameraY);
+
+	for (int k = 0; k < LAYERS; k++) {
+		for (int i = 0; i < LINES; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				tileSet->render(tileMatrix[k][i][j],
+						(j * tileWidth) + actualPosition.x,
+						(i * tileHeight) + actualPosition.y);
+			}
+		}
+	}
+}
+
+void TileMap::setPosition(Vector2 newPosition) {
+	this->position = newPosition;
+}
--- src/TileMap.h
+++ src/TileMap.h
@@ -9,16 +9,31 @@
 #define TILEMAP_H_
 
 #include "TileSet.h"
+#include "Vector2.h"
 #include <string>
+#include <stdio.h>
+
+#define LINES 20
+#define COLUMNS 100
+#define LAYERS 3
 
 class TileMap {
 private:
 	TileSetPtr tileSet;
+	int tileWidth;
+	int tileHeight;
+	Vector2 position;
+	int tileMatrix[LAYERS][LINES][COLUMNS];
+
+	void load(std::string fileName);
 
 public:
 	TileMap(std::string fileName, std::string tileSetFileName, int tileWidth,
 			int tileHeight);
 	virtual ~TileMap();
+
+	void render(float cameraX, float cameraY);
+	void setPosition(Vector2 newPosition);
 };
 
 #endif /* TILEMAP_H_ */
