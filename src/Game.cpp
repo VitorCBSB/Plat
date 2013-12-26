e9c92ba97db728016eb891f0ccd9263987a0f5ac
Vitor Coimbra <vitorcoliveira91@gmail.com>
2013-12-26 11:55:14 -0200

    Arrumado delay do main loop.

--- src/Game.cpp
+++ src/Game.cpp
@@ -23,8 +23,8 @@ int Game::run() {
 			new TileMap(std::string("../maps/testMap.map"),
 					std::string("../tiles/Tileset.png"), TILE_WIDTH,
 					TILE_HEIGHT));
-	double dt = 1.0 / FPS;
 	CameraPtr camera(new Camera());
+	double dt = 1.0 / FPS;
 
 	while (!InputManager::get()->QuitGame()) {
 		InputManager::get()->update();
@@ -35,7 +35,7 @@ int Game::run() {
 		testObj->render(camera->getX(), camera->getY());
 		SDLBase::updateScreen();
 		SDLBase::clearScreen();
-		SDL_Delay(1 / FPS);
+		SDL_Delay((1.0 / FPS) * 1000);
 
 	}
 
