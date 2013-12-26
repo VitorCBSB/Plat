78bdcf4cd42600ba8940e38763720d450b04b622
Vitor Coimbra <vitorcoliveira91@gmail.com>
2013-12-26 14:53:44 -0200

    Classe abstrata para tiles. Sera usada principalmente para implementar tiles com respostas de colisao distintas.

---
+++ src/Tile.cpp
@@ -0,0 +1,25 @@
+/*
+ * Tile.cpp
+ *
+ *  Created on: 26/12/2013
+ *      Author: vitor
+ */
+
+#include "Tile.h"
+
+Tile::Tile(Rect hitBox, TileSetPtr tileSet, int tileIndex) :
+		hitBox(hitBox), tileSet(tileSet), tileIndex(tileIndex) {
+
+}
+
+Tile::~Tile() {
+}
+
+bool Tile::collidesWith(TestObjPtr player) {
+	return hitBox.collidesWith(player->getHitbox());
+}
+
+void Tile::render(float cameraX, float cameraY) {
+	tileSet->render(tileIndex, hitBox.position.x - cameraX,
+			hitBox.position.y - cameraY);
+}
---
+++ src/Tile.h
@@ -0,0 +1,34 @@
+/*
+ * Tile.h
+ *
+ *  Created on: 26/12/2013
+ *      Author: vitor
+ */
+
+#ifndef TILE_H_
+#define TILE_H_
+
+#include "Sprite.h"
+#include "TileSet.h"
+#include "TestObj.h"
+#include "Rect.h"
+#include <tr1/memory>
+
+class Tile {
+private:
+	Rect hitBox;
+	TileSetPtr tileSet;
+	int tileIndex;
+
+public:
+	Tile(Rect hitBox, TileSetPtr tileSet, int tileIndex);
+	virtual ~Tile();
+
+	virtual bool collidesWith(TestObjPtr player);
+	virtual void onCollision(TestObjPtr player) = 0;
+	virtual void render(float cameraX, float cameraY) = 0;
+};
+
+typedef std::tr1::shared_ptr<Tile> TilePtr;
+
+#endif /* TILE_H_ */
